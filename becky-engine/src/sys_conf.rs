8dd0867f5df1a8f71ee4297bd058485b0201ab7f
Adam Flott <adam@adamflott.com>
2026-06-07 19:39:07 -0400

    engine: add failures to SystemConfiguration::new()

--- becky-engine/src/sys_conf.rs
+++ becky-engine/src/sys_conf.rs
@@ -28,29 +28,35 @@ impl SystemConfiguration {
     ///
     /// Directory creation errors are currently ignored and should be handled by
     /// later storage operations.
-    pub async fn new(run_path: PathBuf, emulator_paths: Vec<PathBuf>, vm_root_path: PathBuf, vm_data_root_path: PathBuf, os_cache_root_path: PathBuf) -> Self {
+    pub async fn new(
+        run_path: PathBuf,
+        emulator_paths: Vec<PathBuf>,
+        vm_root_path: PathBuf,
+        vm_data_root_path: PathBuf,
+        os_cache_root_path: PathBuf,
+    ) -> Result<Self, std::io::Error> {
         for path in &emulator_paths {
             if !path.exists() {
                 warn!("system emulator path {:?} does not exist", path);
             }
         }
 
-        let _ = tokio::fs::create_dir_all(&run_path).await;
+        tokio::fs::create_dir_all(&run_path).await?;
         debug!("created path {:?}", run_path);
-        let _ = tokio::fs::create_dir_all(&vm_root_path).await;
+        tokio::fs::create_dir_all(&vm_root_path).await?;
         debug!("created vm run path {:?}", vm_root_path);
-        let _ = tokio::fs::create_dir_all(&vm_data_root_path).await;
+        tokio::fs::create_dir_all(&vm_data_root_path).await?;
         debug!("created vm data path {:?}", vm_data_root_path);
-        let _ = tokio::fs::create_dir_all(&os_cache_root_path).await;
+        tokio::fs::create_dir_all(&os_cache_root_path).await?;
         debug!("created os cache data path {:?}", os_cache_root_path);
 
-        SystemConfiguration {
+        Ok(SystemConfiguration {
             emulator_paths: emulator_paths.clone(),
             binary_paths: vec![],
             run_path: run_path.clone(),
             vm_root_path,
             vm_data_root_path,
             os_cache_root_path,
-        }
+        })
     }
 }
